84607f9053ce1838af2092a302769e330cd212d2
Sai Kumar <kasarla@usc.edu>
2024-01-10 18:49:57 +0530

    Verfication Page Added

---
+++ src/screens/VerificationScreen.jsx
@@ -0,0 +1,77 @@
+import { Text, View, TextInput, SafeAreaView, Pressable } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
+import { useNavigation } from '@react-navigation/native';
+import { StatusBar } from 'react-native-web';
+import { useState } from 'react';
+import GradientButton from '../utils/GradientButton';
+import { AntDesign } from '@expo/vector-icons';
+
+export default function VerificationScreen() {
+
+    const navigation = useNavigation();
+    const [countryCode, setCountryCode] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [hasError, setHasError] = useState(false);
+
+    const changePhoneNumber = (value) => {
+        setPhoneNumber(value);
+    }
+
+    const changeCountryCode = (value) => {
+        setCountryCode(value);
+    }
+
+    const checkErrors = () => {
+        if (countryCode.length === 0) {
+            setHasError(true);
+            return;
+        }
+        else if (phoneNumber.length !== 10) {
+            setHasError(true);
+            return;
+        }
+        else {
+            setHasError(false);
+            navigation.navigate("Home");
+        }
+    }
+
+    const placeholder = {
+        label: 'CC',
+        value: "",
+    };
+
+    const options = [
+        { label: "IN +91", value: '+91' },
+        { label: "US +1", value: '+1' },
+        { label: "CA +7", value: '+7' },
+    ];
+
+    return (
+        <SafeAreaView>
+            <View className="flex justify-items-start bg-white text-black h-screen pt-[8%]">
+                <Text className="flex text-left font-bold text-2xl pl-[4%] pb-[6%]">Enter your Number</Text>
+                <Text className="flex text-left px-[4%] pb-[14%]">We will need to verify your identify to ensure the safety of our users</Text>
+                <View className="flex flex-row justify-start px-[4%] pb-[2%]">
+                    <View className="flex flex-row justify-between">
+                        <View className="flex flex-row justify-center w-[28%] px-4 py-4 bg-neutral-100 rounded-lg border border-slate-300 text-black text-center">
+                            <RNPickerSelect
+                                placeholder={placeholder}
+                                items={options}
+                                onValueChange={changeCountryCode}
+                                value={countryCode}
+                            />
+                            <View className="flex px-[6%] justify-around" />
+                            <AntDesign name="down" size={20} color="black" />
+                        </View>
+                        <TextInput className="w-[68%] h-[100%] px-4 bg-neutral-100 rounded-lg border border-slate-300" onChangeText={changePhoneNumber} value={phoneNumber} placeholder="Phone number" keyboardType="numeric" />
+                    </View>
+                </View>
+                {hasError && <Text className="flex text-left px-[6%] text-red-700">Please enter all the fields!</Text>}
+                <GradientButton onPress={checkErrors} label="Verify" />
+                <StatusBar style="auto" />
+            </View>
+        </SafeAreaView>
+    )
+}
+

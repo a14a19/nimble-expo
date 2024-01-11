7be82d9b3e0b7f5b1cc71e5651ad0bf078761953
Sai Kumar <43190921+saikumar1767@users.noreply.github.com>
2024-01-11 16:13:30 +0530

    Picker Select Fix (#7)

--- src/screens/VerificationScreen.jsx
+++ src/screens/VerificationScreen.jsx
@@ -1,8 +1,8 @@
-import { Text, View, TextInput, SafeAreaView, Pressable } from 'react-native';
+import { Text, View, TextInput, SafeAreaView, Pressable, TouchableWithoutFeedback } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'react-native-web';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import GradientButton from '../utils/GradientButton';
 import { AntDesign } from '@expo/vector-icons';
 
@@ -12,6 +12,7 @@ export default function VerificationScreen() {
     const [countryCode, setCountryCode] = useState("");
     const [phoneNumber, setPhoneNumber] = useState("");
     const [hasError, setHasError] = useState(false);
+    const pickerRef = useRef(null);
 
     const changePhoneNumber = (value) => {
         setPhoneNumber(value);
@@ -45,7 +46,14 @@ export default function VerificationScreen() {
         { label: "IN +91", value: '+91' },
         { label: "US +1", value: '+1' },
         { label: "CA +7", value: '+7' },
+        { label: "UAE +231", value: '231'},
     ];
+
+    const openDrawer = () => {
+        if (pickerRef && pickerRef.current) {
+            pickerRef.current.togglePicker();
+        }
+    }
 
     return (
         <SafeAreaView>
@@ -54,17 +62,21 @@ export default function VerificationScreen() {
                 <Text className="flex text-left px-[4%] pb-[14%]">We will need to verify your identify to ensure the safety of our users</Text>
                 <View className="flex flex-row justify-start px-[4%] pb-[2%]">
                     <View className="flex flex-row justify-between">
-                        <View className="flex flex-row justify-center w-[28%] px-4 py-4 bg-neutral-100 rounded-lg border border-slate-300 text-black text-center">
-                            <RNPickerSelect
-                                placeholder={placeholder}
-                                items={options}
-                                onValueChange={changeCountryCode}
-                                value={countryCode}
-                            />
-                            <View className="flex px-[6%] justify-around" />
-                            <AntDesign name="down" size={20} color="black" />
-                        </View>
-                        <TextInput className="w-[68%] h-[100%] px-4 bg-neutral-100 rounded-lg border border-slate-300" onChangeText={changePhoneNumber} value={phoneNumber} placeholder="Phone number" keyboardType="numeric" />
+                        <TouchableWithoutFeedback onPress={openDrawer}>
+                            <View className="flex flex-row justify-center w-[24%] px-4 py-4 bg-neutral-100 rounded-lg border border-slate-300 text-black text-center">
+                                <RNPickerSelect
+                                    placeholder={placeholder}
+                                    items={options}
+                                    onValueChange={changeCountryCode}
+                                    value={countryCode}
+                                    ref={pickerRef}
+                                />
+                                <View className="pl-2" >
+                                    <AntDesign name="down" size={20} color="#BBBBD1" />
+                                </View>
+                            </View>
+                        </TouchableWithoutFeedback>
+                        <TextInput className="w-[72%] h-[100%] px-4 bg-neutral-100 rounded-lg border border-slate-300" onChangeText={changePhoneNumber} value={phoneNumber} placeholder="Phone number" keyboardType="numeric" />
                     </View>
                 </View>
                 {hasError && <Text className="flex text-left px-[6%] text-red-700">Please enter all the fields!</Text>}

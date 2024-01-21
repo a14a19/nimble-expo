cf02e958a33b2676550f526310e30139ce46b777
Anas Ahmed <anas31197@gmail.com>
2024-01-21 15:03:00 +0530

    Add forms redux (#36)
    * Multiple files changed
    
    Added payloads from LookingToFindScreen and SexualOrientationScreen to Redux formSlice
    
    * Initial Changes
    
    ---------
    
    Co-authored-by: Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,64 +1,65 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import { userSignIn } from '../../services/api';
-import axios from 'axios';
+import { userSignIn } from "../../services/api";
+import axios from "axios";
 
-const url = process.env.EXPO_PUBLIC_API_URL
+const url = process.env.EXPO_PUBLIC_API_URL;
 
 const initialState = {
-    isLoggedIn: false,
-    userData: {},
-    token: "",
-    errors: []
-    
+  isLoggedIn: false,
+  userData: {},
+  token: "",
+  errors: [],
+};
 
-}
-
-export const userSignInApi = createAsyncThunk("auth/userSignIn", async (payload, thunkAPI) => {
+export const userSignInApi = createAsyncThunk(
+  "auth/userSignIn",
+  async (payload, thunkAPI) => {
     try {
-        // console.log("AXIOS", payload);
-        return userSignIn(payload.body, payload.params, payload.options).then((res) => res.data).catch((e) => e.response.data);
+      console.log("AXIOS", payload);
+      return userSignIn(payload.body, payload.params, payload.options)
+        .then((res) => res.data)
+        .catch((e) => e.response.data);
     } catch (e) {
-        thunkAPI.rejectWithValue("Error - ", e)
-        return e;
+      console.log(e);
+      thunkAPI.rejectWithValue("Error - ", e);
+      return e;
     }
-})
+  }
+);
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        handleLogin: (state) => {
-            console.log(state)
-            state.isLoggedIn = !state.isLoggedIn
-        }
+  name: "auth",
+  initialState,
+  reducers: {
+    handleLogin: (state) => {
+      console.log(state);
+      state.isLoggedIn = !state.isLoggedIn;
     },
-    extraReducers: (builder) => {
-        builder.addCase(userSignInApi.fulfilled, (state, { payload }) => {
-            console.log("Fulfilled", payload)
-            if (payload.errors) {
-                state.errors = payload.errors
-                state.isLoggedIn = payload.status
-                state.userData = {}
-                state.token = ""
-                
-            } else {
-                state.userData = payload.data
-                state.errors = []
-                state.token = payload.token
-                state.isLoggedIn = payload.status
-            }
-
-
-        })
-        builder.addCase(userSignInApi.rejected, (state, { payload }) => {
-            console.log("Rejected", payload)
-        })
-        builder.addCase(userSignInApi.pending, (state, { payload }) => {
-            console.log("Pending", payload)
-        })
-    }
-})
+  },
+  extraReducers: (builder) => {
+    builder.addCase(userSignInApi.fulfilled, (state, { payload }) => {
+      console.log("Fulfilled", payload);
+      if (payload.errors) {
+        state.errors = payload.errors;
+        state.isLoggedIn = payload.status;
+        state.userData = {};
+        state.token = "";
+      } else {
+        state.userData = payload.data;
+        state.errors = [];
+        state.token = payload.token;
+        state.isLoggedIn = payload.status;
+      }
+    });
+    builder.addCase(userSignInApi.rejected, (state, { payload }) => {
+      console.log("Rejected", payload);
+    });
+    builder.addCase(userSignInApi.pending, (state, { payload }) => {
+      console.log("Pending", payload);
+    });
+  },
+});
 
 export const { handleLogin } = authSlice.actions;
 
